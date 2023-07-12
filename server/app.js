7375fcb2ad35c0a10525540b61c8bdb5db401524
Dev19Carmona <camilocr3@hotmail.com>
2023-07-12 15:37:01 -0500

    Deploy 3

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
-import "dotenv/config"
+import dotenv from 'dotenv';
+dotenv.config();
 import express from "express";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import { WebSocketServer } from "ws";
